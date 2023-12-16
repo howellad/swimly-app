8ec234a1cc88c0b33c7b8e456bb6256ba6e95559
howellad <adster.howell@gmail.com>
2023-12-16 19:50:16 +0000

    adding another tab and messing with icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -47,7 +47,14 @@ export default function TabLayout() {
         name="two"
         options={{
           title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="info" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="three"
+        options={{
+          title: 'Tab Three',
+          tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />,
         }}
       />
     </Tabs>
